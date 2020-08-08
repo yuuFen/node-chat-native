d0e20bcc350805e4a35872ae229bb25a03b04c6e
Yu <me@yuufen.com>
2020-08-09 01:55:26 +0800

    EditThread布局，flex:1代替绝对布局，添加图片栏，可能IOS需要额外处理键盘事件

--- src/pages/Home/EditThreadScreen.js
+++ src/pages/Home/EditThreadScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {SafeAreaView, TouchableOpacity, TextInput, View, StyleSheet} from 'react-native';
+import {SafeAreaView, TouchableOpacity, TextInput, View, StyleSheet, Image, Dimensions} from 'react-native';
 import {Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction, useTheme} from '@ui-kitten/components';
 
 import {CloseIcon} from '../../components/Icons';
@@ -9,6 +9,8 @@ export default EditThreadScreen = ({navigation}) => {
   const theme = useTheme();
   const [value, setValue] = useState('');
   // const setValue = debounce(setValue1);
+  const windowWidth = Dimensions.get('window').width;
+  const imageContainerWidth = (windowWidth - globalStyles.paddingHorizontal * 2) / 4;
 
   const navigateBack = () => {
     navigation.goBack();
@@ -23,35 +25,56 @@ export default EditThreadScreen = ({navigation}) => {
       </Text>
     </TouchableOpacity>
   );
+
+  const renderImages = (imgs) => {
+    return (
+      <View style={styles.images}>
+        {imgs.map((item, index) => (
+          <View key={index} style={[styles.imageContainer, {width: imageContainerWidth, height: imageContainerWidth}]}>
+            <View style={styles.image}></View>
+          </View>
+        ))}
+        {imgs.length < 4 && (
+          <View style={[styles.imageContainer, {width: imageContainerWidth, height: imageContainerWidth}]}>
+            <View style={styles.image}></View>
+          </View>
+        )}
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <TopNavigation title="发布动态" alignment="center" accessoryLeft={BackAction} accessoryRight={renderRightActions} />
       <Divider />
-      <Layout style={{height: '100%', alignItems: 'center'}}>
+      <Layout style={styles.rootContainer}>
         <TextInput
           style={styles.input}
           placeholder="写点好玩的吧 ლ(╹◡╹ლ )"
           editable={true} //是否可编辑
           multiline={true} //多行输入
-          maxLength={400}
+          maxLength={1000} // 最多输入 600，超出提示标红
           value={value}
           onChangeText={(text) => setValue(text)}
         />
-      </Layout>
-      <View style={[styles.bottomContainer, {borderColor: theme['border-basic-color-3']}]}>
-        
-        <View style={{flexDirection: 'row'}}>
-          <TouchableOpacity style={{paddingHorizontal: 12}}>
-            <Icon style={styles.bottomIcon} fill={theme['text-basic-color']} name="image-outline" />
-          </TouchableOpacity>
-          <TouchableOpacity style={{paddingHorizontal: 12}}>
-            <Icon style={styles.bottomIcon} fill={theme['text-basic-color']} name="at-outline" />
-          </TouchableOpacity>
+        <View>
+          {renderImages([1, 2])}
+
+          <View style={[styles.bottomContainer, {borderColor: theme['border-basic-color-3']}]}>
+            <View style={{flexDirection: 'row'}}>
+              <TouchableOpacity style={{paddingHorizontal: 12}}>
+                <Icon style={styles.bottomIcon} fill={theme['text-basic-color']} name="image-outline" />
+              </TouchableOpacity>
+              <TouchableOpacity style={{paddingHorizontal: 12}}>
+                <Icon style={styles.bottomIcon} fill={theme['text-basic-color']} name="at-outline" />
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={[styles.bottomBtn, {borderColor: theme['text-basic-color']}]}>
+              <Text>+ 圈子</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <TouchableOpacity style={[styles.bottomBtn, {borderColor: theme['text-basic-color']}]}>
-          <Text>+ 圈子</Text>
-        </TouchableOpacity>
-      </View>
+      </Layout>
     </SafeAreaView>
   );
 };
@@ -60,22 +83,38 @@ const BOTTOM_HEIGHT = 60;
 const BOTTOM_ITEM_HEIGHT = 32;
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    // alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   input: {
     overflow: 'hidden',
-    position: 'absolute',
-    top: 0,
-    bottom: BOTTOM_HEIGHT + 60,
-    // height: 200,
+    flex: 1,
     width: '100%',
-
     fontSize: 16,
     textAlignVertical: 'top',
     paddingHorizontal: globalStyles.paddingHorizontal,
     paddingVertical: globalStyles.paddingVertical,
   },
+  images: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: globalStyles.paddingHorizontal,
+    paddingVertical: globalStyles.paddingVertical,
+  },
+  imageContainer: {
+    padding: 3,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#525',
+  },
+
   bottomContainer: {
-    position: 'absolute',
-    bottom: 0,
     height: BOTTOM_HEIGHT,
     width: '100%',
     alignItems: 'center',
