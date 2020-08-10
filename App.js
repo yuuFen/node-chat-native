b29002066adf9ffb6602067426c45c44d5d29fd4
YuuFen <yuufen@YuuFendeiMac.lan>
2020-08-10 09:56:07 +0800

    修改StatusBar跟随主题切换，适配IOS、安卓

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {ApplicationProvider, Button, Icon, IconRegistry, Layout, Text} from '@ui-kitten/components';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import {ApplicationProvider, IconRegistry, useTheme} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
@@ -8,19 +8,31 @@ import {Provider, connect} from 'react-redux';
 import AppNavigator from './src/pages';
 import store from './src/store';
 
-const AppChild = connect((state) => ({theme: state.theme}))((props) => {
+const AppSonSon = connect((state) => ({themeOption: state.theme}))(({themeOption}) => {
+  const theme = useTheme();
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva[props.theme.theme]}>
+    <View style={{backgroundColor: theme['background-basic-color-1'], flex: 1}}>
+      <StatusBar barStyle={themeOption.theme === 'light' ? 'dark-content' : 'light-content'} backgroundColor={theme['background-basic-color-1']} />
+      <SafeAreaView style={{flex: 1}}>
         <AppNavigator />
+      </SafeAreaView>
+    </View>
+  );
+});
+
+const AppSon = connect((state) => ({theme: state.theme}))(({theme}) => {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva[theme.theme]}>
+        <AppSonSon />
       </ApplicationProvider>
-    </SafeAreaView>
+    </>
   );
 });
 
 export default App = () => (
   <Provider store={store}>
-    <AppChild />
+    <AppSon />
   </Provider>
 );
